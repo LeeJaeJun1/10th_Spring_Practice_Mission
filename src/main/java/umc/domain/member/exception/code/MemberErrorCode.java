6294001c7bf71a140115ea93dccc6fdf569bb277
LeeJaeJun1 <jj030207@naver.com>
2026-05-27 02:51:12 +0900

    feat: JWT 로그인 요청/응답 DTO 및 예외 처리 코드 추가

--- src/main/java/umc/domain/member/exception/code/MemberErrorCode.java
+++ src/main/java/umc/domain/member/exception/code/MemberErrorCode.java
@@ -12,7 +12,9 @@ public enum MemberErrorCode implements BaseErrorCode {
 
 	MEMBER_NOT_FOUND(HttpStatus.NOT_FOUND, "MEMBER404_1", "해당 사용자를 찾을 수 없습니다."),
 
-	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "MEMBER409_1", "이미 존재하는 이메일입니다.");
+	EMAIL_ALREADY_EXISTS(HttpStatus.CONFLICT, "MEMBER409_1", "이미 존재하는 이메일입니다."),
+
+	INVALID_PASSWORD(HttpStatus.BAD_REQUEST, "MEMBER400_1", "비밀번호가 일치하지 않습니다");
 
 	private final HttpStatus status;
 	private final String code;
